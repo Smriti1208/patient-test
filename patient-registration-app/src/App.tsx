a36504e17b6531b9e145c09fa62cf5c5c77b3fa3
Smriti K Vantkar <smritivantkar@gmail.com>
2025-05-06 00:34:11 +0530

    Feature: "Created landing page"

--- patient-registration-app/src/App.tsx
+++ patient-registration-app/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import PatientForm from './components/PatientForm';
+import PatientTable from './components/PatientTable';
+import SQLQuery from './components/SQLQuery';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [refresh, setRefresh] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Patient Registration</h1>
+      <PatientForm onRegister={() => setRefresh(!refresh)} />
+        <h2>Table</h2>
+      <PatientTable refresh={refresh} />
+      <h2>Custom SQL Query</h2>
+      <SQLQuery />
+    </div>
+  );  
 }
-
-export default App
